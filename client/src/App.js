f83306e022c2920c57dd700ce166d67e71dbc966
Lindax28 <lindax28@hotmail.com>
2021-12-28 03:45:03 -0800

    Set up frontend websocket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+
+import {
+  connect,
+  sendMessage,
+} from './api';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        connect();
+    }
+
+    send() {
+        console.log("hello");
+        sendMessage("hello");
+      }
+
+    render() {
+        return(
+            <div className="App">
+                <button onClick={this.send}>Send</button>
+            </div>
+        )
+    }
 }
 
 export default App;
