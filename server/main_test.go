0e338d4cb7c907fbe9cdf3ee0cf561424a200810
Lindax28 <lindax28@hotmail.com>
2021-12-28 04:34:13 -0800

    Add test for websocket handler

--- server/main_test.go
+++ server/main_test.go
@@ -4,7 +4,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/gorilla/websocket"
 )
 
 func TestHandler(t *testing.T) {
@@ -23,6 +26,31 @@ func TestHandler(t *testing.T) {
 	actual := recorder.Body.String()
 	if actual != expected {
 		t.Errorf("handler returned incorrect body: got %v, expected %v", actual, expected)
+	}
+}
+
+func TestWebsocketHandler(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(websocketHandler))
+	defer s.Close()
+	u := "ws" + strings.TrimPrefix(s.URL, "http")
+	ws, resp, err := websocket.DefaultDialer.Dial(u, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 3; i++ {
+		if err := ws.WriteMessage(websocket.TextMessage, []byte("hello")); err != nil {
+			t.Fatal(err)
+		}
+		_, p, err := ws.ReadMessage()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if actual, expected := resp.StatusCode, http.StatusSwitchingProtocols; actual != expected {
+			t.Errorf("handler returned incorrect body: got %v, expected %v", actual, expected)
+		}
+		if actual, expected := string(p), "hello"; actual != expected {
+			t.Errorf("handler returned incorrect message: got %v, expected %v", actual, expected)
+		}
 	}
 }
 
