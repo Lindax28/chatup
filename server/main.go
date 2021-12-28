ff7cf573dab41138feec677d1e63ad3654ffaaa8
Lindax28 <lindax28@hotmail.com>
2021-12-28 03:04:49 -0800

    Set up React client and websocket endpoint

--- server/main.go
+++ server/main.go
@@ -6,12 +6,35 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
+	r.HandleFunc("/ws", websocketHandler).Methods("GET")
 	return r
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
 }
 
 func main() {
@@ -24,4 +47,14 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, r.Host)
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	reader(conn)
 }
